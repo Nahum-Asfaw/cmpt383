51271346c53dcd6521440dd356d6e0f9a0380c91
nahum <naa93@sfu.ca>
2026-09-21 16:15:48 -0700

    Finished fib.py

---
+++ python/gicd/fib.py
@@ -0,0 +1,67 @@
+# Python_practice
+# Q1
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+class Fib:
+    def __init__(self, n):
+        self.n = n
+        self.index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.index < self.n:
+            self.index += 1
+            return fib(self.index -1)
+        else:
+            raise StopIteration
+
+def testFib(n):
+    for i in Fib(n):
+        print(i)
+#------------------------------
+testFib(10)
+#------------------------------
+# Q2
+def fib_gen(n):
+    for x in range(0, n):
+        yield fib(x)
+    
+def testFib2(n):
+    for i in fib_gen(n):
+        print(i)
+#-----------------------------
+testFib2(10)    
+#-----------------------------
+# Q3
+def take_any(f): # prints the func name, parameters, and return value
+    def print_stuff(*args, **kwargs):
+        print(f'{f.__name__} called with arg(s):', *args)
+        this = f(*args, **kwargs)
+        print("function returned: ")
+        return this
+    return print_stuff
+    
+def _fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return _fib(n-1) + _fib(n-2)
+
+@take_any
+def fib_rec(n):
+    return _fib(n)
+
+def testFib3(n):
+    print(fib_rec(n))
+#----------------------------
+testFib3(10)
+#----------------------------
+# Done.
